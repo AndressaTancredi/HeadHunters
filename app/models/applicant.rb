8e8d8432798812763d367edf8e97520d4fb51b75
AndressaTancredi <andressa.tancredi@hotmail.com>
2020-07-02 22:31:34 -0300

    Resize Thumbnail

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -1,4 +1,8 @@
 class Applicant < ApplicationRecord
   has_one_attached :avatar
   belongs_to :applicantuser
+
+  def thumbnail
+    return self.avatar.variant(resize: '250x250!').processed
+  end
 end
